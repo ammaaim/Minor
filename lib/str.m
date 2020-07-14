a0183b4bff8fb50089e7f6497b9810cd627bac3b
Alex Balan <alex@MacBook-Air.local>
2020-07-14 12:11:26 +0300

    minor changes

--- lib/str.m
+++ lib/str.m
@@ -4,14 +4,13 @@
 let STR_TERM = 0
 
 
-
-let str_new = func (len : Nat) -> Str {return malloc(len) to Str}
+let str_new = func (len : Nat) -> Str {return malloc(len + 1) to Str}
 
 
 // create copy of string
 let dup = func (s : Str) -> Str {
   let len = strlen(s)
-  let new_s = str_new(len + 1)
+  let new_s = str_new(len)
   strcpy(new_s, s)
   return new_s
 }
@@ -24,7 +23,7 @@ let cat = func (s1, s2 : Str) -> Str {
 
   let len12 = len1 + len2
 
-  let s = str_new(len12 + 1)
+  let s = str_new(len12)
 
   strcpy(&s[0] to Str, s1)
   strcpy(&s[len1] to Str, s2)
@@ -43,7 +42,7 @@ let cat3 = func (s1, s2, s3 : Str) -> Str {
   let len12 = len1 + len2
   let len123 = len12 + len3
 
-  let s = str_new(len123 + 1)
+  let s = str_new(len123)
 
   strcpy(&s[0] to Str, s1)
   strcpy(&s[len1] to Str, s2)
@@ -65,7 +64,7 @@ let cat4 = func (s1, s2, s3, s4 : Str) -> Str {
   let len123 = len12 + len3
   let len1234 = len123 + len4
 
-  let s = str_new(len1234 + 1)
+  let s = str_new(len1234)
 
   strcpy(&s[0] to Str, s1)
   strcpy(&s[len1] to Str, s2)
@@ -90,7 +89,7 @@ let cat5 = func (s1, s2, s3, s4, s5 : Str) -> Str {
   let len1234 = len123 + len4
   let len12345 = len1234 + len5
 
-  let s = str_new(len12345 + 1)
+  let s = str_new(len12345)
 
   strcpy(&s[0] to Str, s1)
   strcpy(&s[len1] to Str, s2)
